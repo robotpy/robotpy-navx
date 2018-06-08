db4ba59392d6ede24e383b4ab17e19bf566080be
Dustin Spicuzza <dustin@virtualroadside.com>
2018-06-07 22:26:15 -0400

    Add tests

---
+++ tests/conftest.py
@@ -0,0 +1,45 @@
+
+# TODO: remove this once WPILib is public, and use the real thing
+
+import sys
+
+import pytest
+from unittest.mock import MagicMock, call
+
+class FakeSensorBase:
+    pass
+
+def pytest_runtest_setup():
+    import networktables
+    networktables.NetworkTables.startTestMode()
+    
+def pytest_runtest_teardown():
+    import networktables
+    networktables.NetworkTables.shutdown()
+    
+    from wpilib import SmartDashboard
+    SmartDashboard._reset()
+
+@pytest.fixture(scope="function")
+def wpimock(monkeypatch):
+    mock = MagicMock(name='wpimock')
+    mock.SensorBase = FakeSensorBase
+    monkeypatch.setitem(sys.modules, 'wpilib', mock)
+    return mock
+
+@pytest.fixture(scope='function')
+def wpitime(monkeypatch):
+    
+    class FakeTime:
+        def __init__(self):
+            self.now = 0 # in seconds
+            
+        def getFPGATime(self):
+            return self.now * 1000000
+            
+    ft = FakeTime()
+    
+    import hal
+    monkeypatch.setattr(hal, 'getFPGATime', ft.getFPGATime)
+    
+    return ft
---
+++ tests/run_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -e
+
+cd $(dirname $0)
+
+export PYTHONPATH=".."
+
+if [ "$RUNCOVERAGE" == "1" ]; then
+    python3 -m coverage run --source navx -m pytest "$@"
+    python3 -m coverage report -m
+else
+    python3 -m pytest "$@"
+fi
+
+# Run tests on examples repository
+if [ "$CONTINUOUS_INTEGRATION" == "true" ]; then
+    curl https://raw.githubusercontent.com/robotpy/examples/master/_remote_tests.sh | bash -s navx
+fi
